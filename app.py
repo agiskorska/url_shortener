cb5da7e13cc4cc1aa55fd283aa488ef338edd448
agiskorska <skorskaga@gmail.com>
2022-09-21 12:01:57 +0100

    Set up DB

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
 import time
-from db import data
+from flask_sqlalchemy import SQLAlchemy
 
 home_url = 'http://127.0.0.1:5000/'
 
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url.db'
+db  = SQLAlchemy(app)
+
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.long_url
 
 @app.get('/')
 def index():
@@ -18,18 +27,24 @@ def shortened():
     if request.method == 'POST':
         url = request.form['long_url']
         rand_time = str(round(time.time()*100000))[-9:]
-        data.append({'long_url': url, 'id': rand_time})
+        new_url = Url(id=rand_time, long_url=url)
+        db.session.add(new_url)
+        db.session.commit()
         shortened_url = home_url + rand_time
-        print(data)
+        print(db)
         return render_template('response.html', title= 'Shorten URL | Response', url=shortened_url)
     else:
         return 'wrong route'
 
 @app.get('/<id>')
 def redirect_to_url(id):
-    url = next((i['long_url'] for i in data if i['id'] == id), None)
+    url = Url.query.filter_by(id=id).first()
     print(url)
     return redirect(url, code=302)
+
+@app.get('/thankyou')
+def thankyou():
+    return '<h1>thank you!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
